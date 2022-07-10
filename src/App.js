2e044c105d94d802db4cdddf98385b2d9dbc2be0
Sarthak Mehra <smehra.office@gmail.com>
2022-07-10 22:31:55 +0530

    Parse Object to Text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const input = {
+    taxi: "a car licensed to transport passengers in return for payment of a fare",
+    food: {
+        sushi:
+            "a traditional Japanese dish of prepared rice accompanied by seafood and vegetables",
+        apple: {
+            Honeycrisp:
+                "an apple cultivar developed at the MAES Horticultural Research Center",
+            Fuji: "an apple cultivar developed by growers at Tohoku Research Station",
+        },
+    },
 }
+
+const App = () => {
+    const displayContent = parseObject(input);
+    return (<div dangerouslySetInnerHTML={{ __html: displayContent }}></div>)
+};
+
+const parseObject = obj => {
+    let result = '<ul style="list-style-type: none">';
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            let item = `<li>${key}: ${typeof obj[key] == 'object' ? parseObject(obj[key]) : obj[key]} </li>`;
+
+            result += item;
+        }
+    }
+    result += '</ul>';
+    return result;
+};
 
 export default App;
